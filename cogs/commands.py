ff9fa77724fefe4059e9a2ddeaa09169028ac9ec
CyberKnight007 <togedemaru.1707@gmail.com>
2021-01-13 13:44:38 +0530

    added cogs

---
+++ cogs/commands.py
@@ -0,0 +1,91 @@
+from discord.ext import commands
+import discord
+import random
+import time
+
+
+class CommandsCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.bot.remove_command('help')
+
+    @commands.command(name="signup")
+    async def signup(self, ctx):
+        embed = discord.Embed(
+            title="Sign Up", description="Go to https://thetahacks.tech/signup to sign up. Be sure to register by January 15, 2021! Also don't forget to sign up on Devpost at https://thetahacks.devpost.com and for events at https://thetahacks.tech/events.", color=0xb134eb)
+        await ctx.send(embed=embed)
+
+    @commands.command(name="info")
+    async def info(self, ctx):
+        embed = discord.Embed(
+            title="Information", description="ThetaHacks is a 24-hour virtual High-School Hackathon occurring from January 15-18, 2021. What better way to start off your winter break with free merch, coding workshops, and a community of developers to talk with! We have awards from our sponsors ranging from awesome tech to free t-shirts & more! Anyone from any background of coding is welcome to join. \n\nLinks:\nMore info and signups on our website: https://thetahacks.tech \n Devpost: https://thetahacks.devpost.com", color=0xc0e8f9)
+        await ctx.send(embed=embed)
+
+    @commands.command(name="ping")
+    async def ping(self, ctx):
+        start = time.perf_counter()
+        message = await ctx.send("Ping...")
+        end = time.perf_counter()
+        duration = (end - start) * 1000
+        await message.edit(content='Pong! {:.2f}ms'.format(duration))
+
+    @commands.command(name="dice")
+    async def dice(self, ctx, n=1):
+        try:
+            if n < 1 or n > 10:
+                await ctx.send("Invalid arguments for command `dice`.")
+            else:
+                # roll N dice
+                await ctx.send(" ".join(str(random.randint(1, 6)) for i in range(n)))
+        except:  # error
+            await ctx.send("Invalid arguments for command `dice`.")
+
+    @commands.command(name="magic8")
+    async def magic8(self, ctx):
+        bm = ("It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "Most likely.", "Outlook good.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.",
+              "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful.")
+        await ctx.send(random.choice(bm))
+
+    @commands.command(name="help")
+    async def help(self, ctx):
+        embed = discord.Embed(
+            title="Help", description="Valid commands:\n\n**Utility**\n`!signup` - Signup form link\n`!info` - ThetaHacks information\n`!help` - View valid commands\n`!stats` - See server statistics\n`!rules` - See server rules\n\n**Fun**\n`!kill @user`\n`!ping` - pong\n`!magic8` - Magic 8 ball\n`!dice N` - Roll N dice (1 <= N <= 10)", color=0x0027ff)
+        await ctx.send(embed=embed)
+
+    @commands.command(name="rules")
+    async def rules(self, ctx):
+        embed = discord.Embed(
+            title="Server Rules", description="1. Do not bully or harass others. Homophobia, racism and other discrimination is not allowed. Treat others the way you wish to be treated.\n\n2. Spamming, messages that do not contribute to the general conversation and non-English messages are not allowed. With this in mind, please also send content to its relevant channels.\n\n3. Excessive or toxic swearing, as well as generally distasteful or NSFW content is not allowed.\n\n4. Do not partake in activity against any Terms of Service within our community. This includes but is not limited to, the act of purchasing and selling accounts.\n\n5. Do not promote your personal material on our server without consent of a mod or admin. If you would like to partner with us, please contact an admin.\n\n6. Discord statuses/nicknames/names should be clean, this means no slurs, nothing that breaks TOS, no promotion, etc. Failure to comply with a mod’s request to change your status in a timely manner will deem a punishment proportionate to how severe your status is.\n\n7. Logical extensions of rules may also be enforced.", color=0xaa00ff)
+        await ctx.send(embed=embed)
+
+    @commands.command(name="kill")
+    async def kill(self, ctx, member: discord.Member):
+        if not member:
+            return await ctx.send("Invalid arguments for command `kill`")
+        kill_messages = ["barbecued", "disintegrated", "360-no-scoped",
+                         "eaten alive", "yeeted out of existence", "squashed", "smited", "dropped in the void"]
+        # choose random message
+        this_msg = random.choice(kill_messages)
+
+        embed = discord.Embed(
+            title="K-O!", description="%s was %s by %s" % (member.name, this_msg, ctx.author.name), color=0xff00d1)
+        await ctx.send(embed=embed)
+
+    @commands.command(name="stats")
+    async def stats(self, ctx):
+        everyone = ctx.guild.get_role(717170061382516736)
+        attendees = ctx.guild.get_role(721874238801313884)
+        partners = ctx.guild.get_role(741822062221459568)
+        bots = ctx.guild.get_role(721827685990531113)
+        mentors = ctx.guild.get_role(722143200910901250)
+        staff = ctx.guild.get_role(730445847938203718)
+        coordinators = ctx.guild.get_role(717171411692683275)
+
+        text = f"`{len(coordinators.members)}` Coordinators\n`{len(staff.members)}` Staff\n`{len(mentors.members)}` Mentors\n`{len(partners.members)}` Partners\n`{len(bots.members)}` Bots\n`{len(attendees.members)}` Attendees\n`{len(everyone.members)}` All Members"
+        embed = discord.Embed(
+            title="ThetaHacks Stats", description=text, color=0x00ff9d)
+        await ctx.send(embed=embed)
+    
+
+def setup(bot):
+    bot.add_cog(CommandsCog(bot))
---
+++ cogs/events.py
@@ -0,0 +1,398 @@
+import discord
+from discord.ext import commands
+from discord.utils import get
+import random
+
+
+class EventsCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        client = self.bot
+        # set status (currently broken)
+        await client.change_presence(activity=discord.Game(name="thetahacks.tech"))
+        # print when ready
+        print(f'{client.user} has connected to Discord!')
+
+        print(client.guilds[0].emojis)
+
+        py = get(client.guilds[0].emojis, name='python')
+        clang = get(client.guilds[0].emojis, name='clang')
+        cpp = get(client.guilds[0].emojis, name='cpp')
+        java = get(client.guilds[0].emojis, name='java')
+        html = get(client.guilds[0].emojis, name='html')
+        php = get(client.guilds[0].emojis, name='php')
+        js = get(client.guilds[0].emojis, name='javascript')
+        csharp = get(client.guilds[0].emojis, name='csharp')
+        mern = get(client.guilds[0].emojis, name='mern')
+        swift = get(client.guilds[0].emojis, name='swift')
+        dart = get(client.guilds[0].emojis, name='dartlang')
+        other = get(client.guilds[0].emojis, name='huh')
+        rust = get(client.guilds[0].emojis, name='rust')
+
+        client.roledict[py] = "Python"
+        client.roledict[clang] = "Clang"
+        client.roledict[cpp] = "C++"
+        client.roledict[java] = "Java"
+        client.roledict[html] = "HTML/CSS"
+        client.roledict[php] = "PHP"
+        client.roledict[js] = "JavaScript"
+        client.roledict[mern] = "MERN"
+        client.roledict[csharp] = "C#"
+        client.roledict[swift] = "Swift"
+        client.roledict[dart] = "Dart"
+        client.roledict[rust] = "Rust"
+        client.roledict[other] = "Other"
+
+        print(client.roledict)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        client = self.bot
+        if(not client.sent):
+            client.sent = True
+
+            text = "Available Roles:\n\n" + "\n".join(v + ": " + str(
+                k) for k, v in client.roledict2.items()) + "\n\n**Unreact to remove a role.**"
+            embed2 = discord.Embed(
+                title="Get Topic Roles", description=text, color=0x0000ff)
+
+            channel2 = get(message.author.guild.text_channels,
+                           name="get-roles")
+
+            msg2 = await channel2.send(embed=embed2)
+
+            # store message in variable (check line 394)
+            client.v4 = str(msg2.id)
+
+            for key in client.roledict2.keys():
+                await msg2.add_reaction(key)
+
+            text = "Available Roles:\n\n" + "\n".join(v + ": " + str(
+                k) for k, v in client.roledict.items()) + "\n\n**Unreact to remove a role.**"
+
+            embed2 = discord.Embed(
+                title="Get Language Roles", description=text, color=0x00ff00)
+
+            channel2 = get(message.author.guild.text_channels,
+                           name="get-roles")
+
+            msg2 = await channel2.send(embed=embed2)
+
+            # store message in variable (check line 394)
+            client.v2 = str(msg2.id)
+
+            for key in client.roledict.keys():
+                print(key)
+                await msg2.add_reaction(key)
+
+            if message.author == client.user:
+                return
+
+            if "".join([i for i in message.content.lower() if i != " "]) in ('hi', 'hello', 'hola'):
+                await message.channel.send("Hello %s!" % message.author.display_name)
+
+            if message.content.lower().strip().split(" ")[0] == "sudo":
+                # admin/mod.bot role variable
+                admin = get(message.author.guild.roles, name="Admin")
+                # mod = get(message.author.guild.roles, name="Senior Mods")
+                bot = get(message.author.guild.roles, name="ThetaHacks Bot")
+
+                # only admins can use this command
+                if admin in message.author.roles or bot in message.author.roles:
+                    temp = message.content.lower().split(" ")
+
+                    # message
+
+                    if temp[1] == "message":
+                        if temp[2] == "@#de43v%^":
+                            tobesent = " ".join(message.content.split(" ")[3:])
+
+                            print(tobesent)
+                            for member in message.author.guild.members:
+                                try:
+                                    await member.send(("Hello %s,\n\n" % member.mention) + tobesent)
+                                except:
+                                    pass
+                        else:
+                            tobesent = " ".join(message.content.split(" ")[2:])
+
+                            print(tobesent)
+                            for member in message.author.guild.members:
+                                try:
+                                    if admin in member.roles:
+                                        await member.send(("Hello %s,\n\n" % member.mention) + tobesent)
+                                except:
+                                    pass
+
+                    # CLEAR
+
+                    if temp[1] == "clear":
+                        # if no arguments
+                        if len(temp) == 2:
+                            await message.channel.send("Invalid arguments for command `clear`.")
+                        elif temp[2] == "all":
+                            # clear all
+                            msg = []
+                            # create list of all messages in channel history
+                            async for x in message.channel.history():
+                                msg.append(x)
+                            # delete messages
+                            await message.channel.delete_messages(msg)
+                            await message.channel.send("All messages deleted by %s." % message.author.display_name)
+                        else:
+                            try:
+                                # try to parse argument for integer
+                                n = int(temp[2])
+
+                                # if invalid argument
+                                if n <= 0 or not n:
+                                    await message.channel.send("Invalid arguments for command `clear`.")
+                                else:
+                                    # delete last n messages (plus the clear command)
+                                    msg = []
+                                    async for x in message.channel.history(limit=n+1):
+                                        msg.append(x)
+                                    await message.channel.delete_messages(msg)
+
+                                    await message.channel.send("`%i` messages deleted by %s" % (n, message.author.display_name))
+                            except:
+                                # error while parsing
+                                await message.channel.send("Invalid arguments for command `clear`.")
+
+                    # KICK
+
+                    if temp[1] == "kick":
+                        # if no user provided
+                        if len(temp) == 2:
+                            await message.channel.send("Invalid arguments for command `kick`.")
+                        try:
+                            # attempt to parse for user
+                            u = message.mentions[0]
+                        except:
+                            # no mentions found
+                            await message.channel.send("Invalid arguments for command `kick`.")
+                        else:
+                            # possible kick messages
+                            kick_messages = ["kicked in the heinie",
+                                             "yeeted out of existence", "turned into dust", "barbecued"]
+
+                            this_msg = random.choice(kick_messages)
+
+                            if len(temp) > 3:
+                                # if reason is given
+
+                                # get reason
+                                r = " ".join(temp[3:])
+
+                                # cannot kick admin, bot, or mod
+                                if admin in u.roles or bot in u.roles:
+                                    await message.channel.send("Insufficient permissions.")
+                                else:
+                                    # send message in channel
+                                    embed = discord.Embed(
+                                        title="", description="%s was %s by %s.\nReason: %s." % (temp[2], this_msg, message.author.display_name, r), color=0xffa600)
+                                    # send DM to kicked user
+                                    embed2 = discord.Embed(
+                                        title="", description="You were kicked from the `Official ThetaHacks Server` by %s.\nReason: %s." % (message.author.display_name, r), color=0xffa600)
+
+                                    await u.send(embed=embed2)
+
+                                    # kick user
+
+                                    await message.author.guild.kick(user=u, reason=r)
+                                    await message.channel.send(embed=embed)
+                            else:
+                                # if no reason provided
+
+                                # cannot kick admin, bot, or mod
+                                if admin in u.roles or bot in u.roles:
+                                    await message.channel.send("Insufficient permissions.")
+                                else:
+                                    # send message in channel
+                                    embed = discord.Embed(
+                                        title="", description="%s was %s by %s.\nNo reason provided." % (temp[2], this_msg, message.author.display_name), color=0xffa600)
+                                    # send DM to kicked user
+                                    embed2 = discord.Embed(
+                                        title="", description="You were kicked from the `Official ThetaHacks Server` by %s.\nNo reason provided." % (message.author.display_name), color=0xffa600)
+
+                                    # kick user
+
+                                    await u.send(embed=embed2)
+                                    await message.author.guild.kick(user=u)
+                                    await message.channel.send(embed=embed)
+
+                    # BAN
+
+                    if temp[1] == "ban":
+                        # no user provided
+                        if len(temp) == 2:
+                            await message.channel.send("Invalid arguments for command `ban`.")
+                        try:
+                            # parse for user
+                            u = message.mentions[0]
+                        except:
+                            # no mentions
+                            await message.channel.send("Invalid arguments for command `ban`.")
+                        else:
+                            # possible messages
+                            ban_messages = ["struck by the banhammer",
+                                            "banned out of existence"]
+
+                            this_msg = random.choice(ban_messages)
+
+                            # with reason
+                            if len(temp) > 3:
+
+                                # get reason
+                                r = " ".join(temp[3:])
+
+                                # cannot kick admin, bot, or mod
+                                if admin in u.roles or bot in u.roles:
+                                    await message.channel.send("Insufficient permissions.")
+                                else:
+
+                                    # send message and ban
+
+                                    embed = discord.Embed(
+                                        title="", description="%s was %s by %s.\nReason: %s." % (temp[2], this_msg, message.author.display_name, r), color=0xff0000)
+                                    embed2 = discord.Embed(
+                                        title="", description="You were banned from the `Official ThetaHacks Server` by %s. Reason: %s." % (message.author.display_name, r), color=0xff0000)
+                                    await u.send(embed=embed2)
+                                    await message.author.guild.ban(user=u, reason=r)
+                                    await message.channel.send(embed=embed)
+                            else:
+                                # cannot kick admin, bot, or mod
+                                if admin in u.roles or bot in u.roles:
+                                    await message.channel.send("Insufficient permissions.")
+                                else:
+
+                                    # send message and ban
+
+                                    embed = discord.Embed(
+                                        title="", description="%s was %s by %s.\nNo reason provided." % (temp[2], this_msg, message.author.display_name), color=0xff0000)
+                                    embed2 = discord.Embed(
+                                        title="", description="You were banned by %s.\nNo reason provided." % (message.author.display_name), color=0xff0000)
+                                    await u.send(embed=embed2)
+                                    await message.author.guild.ban(user=u)
+                                    await message.channel.send(embed=embed)
+
+                    if temp[1] == "top":
+                        d = {}
+                        msg = []
+
+                        for channel in client.guilds[0].text_channels:
+                            await message.channel.send("Parsing " + channel.name)
+
+                            # create list of all messages in channel history
+                            async for x in channel.history(limit=10000000000000000):
+                                msg.append(x)
+
+                        for m in msg:
+                            if m.author.display_name in d:
+                                d[m.author.display_name] += 1
+                            else:
+                                d[m.author.display_name] = 1
+
+                        d = {k: v for k, v in sorted(
+                            d.items(), key=lambda item: item[1], reverse=True)}
+
+                        with open('d.txt', 'w') as f:
+                            f.write("\n".join(k+" "+str(v)
+                                              for k, v in d.items()))
+
+                        with open('d.txt', 'rb') as f:
+                            await message.channel.send(file=discord.File(f, 'd.txt'))
+
+                else:
+                    # if message author is not not admin/mod/bot
+                    await message.channel.send("{} is not in the sudoers file. This incident will be reported.".format(
+                        message.author.display_name))
+
+            # INVALID ATTEMPTED COMMAND #
+            #
+            #
+            # INVALID ATTEMPTED COMMAND #
+        elif message.content.strip()[0] == '!':
+            await message.channel.send("Invalid command. `!help` for more commands.")
+        await self.bot.process_commands(message)
+
+    @commands.Cog.listener()
+    async def on_reaction_remove(self, reaction, user):
+        client = self.bot
+        if str(reaction.message.id) == client.v2 and reaction.emoji in client.roledict.keys():
+            role = get(user.guild.roles, name=client.roledict[reaction.emoji])
+            await user.remove_roles(role)
+
+            await user.send("Your `" + client.roledict[reaction.emoji] + "` language role has been removed.")
+
+        if str(reaction.message.id) == client.v4 and str(reaction.emoji).strip() in client.roledict2.keys():
+            role = get(user.guild.roles,
+                       name=client.roledict2[str(reaction.emoji).strip()])
+            await user.remove_roles(role)
+
+            await user.send("Your `" + client.roledict2[str(reaction.emoji).strip()] + "` topic role has been removed.")
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        client = self.bot
+        ####### VERIFY WITH REACTION #######
+        #
+        #
+        #
+        ####### VERIFY WITH REACTION #######
+        try:
+            if str(reaction.message.id) == client.v2:
+                if reaction.emoji in client.roledict.keys():
+                    role = get(user.guild.roles,
+                               name=client.roledict[reaction.emoji])
+                    await user.add_roles(role)
+
+                    await user.send("The `" + client.roledict[reaction.emoji] + "` language role has been given to you.")
+                else:
+                    await reaction.remove(user)
+
+            if str(reaction.message.id) == client.v4:
+                if str(reaction.emoji).strip() in client.roledict2.keys():
+                    role = get(
+                        user.guild.roles, name=client.roledict2[str(reaction.emoji).strip()])
+                    await user.add_roles(role)
+
+                    await user.send("The `" + client.roledict2[str(reaction.emoji).strip()] + "` topic role has been given to you.")
+                else:
+                    await reaction.remove(user)
+        except:
+            pass
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        client = self.bot
+        ##### CUSTOM WELCOME #####
+        #
+        #
+        #
+        ##### CUSTOM WELCOME #####
+
+        for channel in member.guild.channels:
+            if channel.name == 'welcome':
+                # send to welcome channel
+                embed = discord.Embed(
+                    title="Welcome", description="Welcome to the `Official ThetaHacks Server`, %s! Sign up for the hackathon at https://thetahacks.tech/signup" % member.mention, color=0xff00d1)
+                await channel.send(embed=embed)
+
+                # send in DMs
+                embed = discord.Embed(
+                    title="Welcome", description="Hello %s, welcome to the `Official ThetaHacks Server`! Sign up for the hackathon at https://thetahacks.tech/signup, join the Devpost at https://thetahacks.devpost.com, and sign up for events at https://thetahacks.tech/events!" % member.mention, color=0xff00d1)
+                await member.send(embed=embed)
+
+                role = get(member.guild.roles, name="Attendees")
+                await member.add_roles(role)
+
+                await member.send("The `Attendee` role has been given to you.")
+                break
+
+
+def setup(bot):
+    bot.add_cog(EventsCog(bot))
